3a196f5a1b0978f766f27d06ede508551710e8ef
LiasCode <liasraul00@gmail.com>
2023-06-11 19:02:51 -0400

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 # /usr/bin/sh
 
-# fasm ./src/main.asm ./built/game-of-tanks.img && qemu-system-x86_64 -drive file=./built/game-of-tanks.img,format=raw
-
-# nasm -fbin ./src/game-of-tanks.asm -o ./built/game-of-tanks.bin && qemu-system-x86_64 -drive file=./built/game-of-tanks.img,format=raw
+make && qemu-system-x86_64 -drive file=./built/game-of-tanks.img,format=raw
